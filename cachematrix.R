4f1d2cde97d271273d0951b6d879498b57c00058
talalf <talal.fazmin1@gmail.com>
2020-07-16 20:17:33 +0100

    Made the first function to create matrix (makeMatrix)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is to create a matrix object that is able to be used to create a cached inverse vector
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inv
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
